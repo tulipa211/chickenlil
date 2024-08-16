8f41d2d4be32681b9392c163a3109f8fd62f00e8
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-08-16 11:16:11 -0300

    a

---
+++ script.js
@@ -0,0 +1,5 @@
+const caixaPrincipal = document.querySelector('caixa-principal');
+const caixaPerguntas = document.querySelector('caixa-pergumtas');
+const caixaAlternativas = document.querySelector('caixa-alternativas');
+const caixaResultado = document.querySelector('caixa-resultado');
+const textoResultado = document.querySelector('texto-principal');
