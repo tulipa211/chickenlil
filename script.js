9d78739e6c752c816312a010b6087b780cbc4c5e
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-08-16 11:50:36 -0300

    a

--- script.js
+++ script.js
@@ -3,3 +3,55 @@ const caixaPerguntas = document.querySelector('caixa-pergumtas');
 const caixaAlternativas = document.querySelector('caixa-alternativas');
 const caixaResultado = document.querySelector('caixa-resultado');
 const textoResultado = document.querySelector('texto-principal');
+
+const pergumtas = [ 
+    {
+        enunciado: "no âmbito social, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmação: "afirmação",
+            },
+        ] 
+    },
+    {
+        enunciado: "no âmbito ambiental, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmação: "afirmação",
+            },
+        ] 
+    },
+    {
+        enunciado: "no âmbito tecnologico, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmação: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmação: "afirmação",
+            },
+        ] 
+    },
+]
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta (){
+    perguntaAtual[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
