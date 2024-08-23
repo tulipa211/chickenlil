aa380379b27edddac769d1011e6abfb88f7525ad
tulipa211 <159141887+tulipa211@users.noreply.github.com>
2024-08-23 12:14:01 -0300

    t

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const caixaPrincipal = document.querySelector('caixa-principal');
-const caixaPerguntas = document.querySelector('caixa-pergumtas');
+const caixaPerguntas = document.querySelector('caixa-perguntas');
 const caixaAlternativas = document.querySelector('caixa-alternativas');
 const caixaResultado = document.querySelector('caixa-resultado');
-const textoResultado = document.querySelector('texto-principal');
+const textoResultado = document.querySelector('texto-resultado');
 
 const perguntas = [ 
     {
